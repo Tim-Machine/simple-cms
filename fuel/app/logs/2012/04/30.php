f6a3671b51de27f6ede6de2f7dfbb81cd1fe6832
tsmith86 <tsmith@bfgcom.com>
2012-06-06 16:56:44 -0400

    first commit

---
+++ fuel/app/logs/2012/04/30.php
@@ -0,0 +1,18 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Error - 2012-04-30 21:00:00 --> Error - SQLSTATE[42S22]: Column not found: 1054 Unknown column 'email' in 'where clause' with query: "SELECT * FROM `users` WHERE `username` = 'admin' OR `email` = 'admin@bfgcom.com'" in /Applications/MAMP/htdocs/cms/fuel/core/classes/database/pdo/connection.php on line 137
+Error - 2012-04-30 21:34:17 --> 2 - Attempt to assign property of non-object in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 25
+Error - 2012-04-30 21:34:17 --> 8 - Undefined variable: data in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 26
+Error - 2012-04-30 21:34:17 --> 2 - Attempt to assign property of non-object in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 26
+Warning - 2012-04-30 21:34:17 --> Fuel\Core\Request::execute - The Controller_home::after() method should accept and return the Controller's response, empty return for the after() method is deprecated.
+Warning - 2012-04-30 21:34:17 --> Fuel\Core\Request::execute - The Controller_home controller should return a string or a Response object, support for the $controller->response object is deprecated.
+Error - 2012-04-30 21:34:46 --> 2 - Attempt to assign property of non-object in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 25
+Error - 2012-04-30 21:34:46 --> 2 - Attempt to assign property of non-object in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 26
+Warning - 2012-04-30 21:34:46 --> Fuel\Core\Request::execute - The Controller_home::after() method should accept and return the Controller's response, empty return for the after() method is deprecated.
+Warning - 2012-04-30 21:34:46 --> Fuel\Core\Request::execute - The Controller_home controller should return a string or a Response object, support for the $controller->response object is deprecated.
+Error - 2012-04-30 21:35:58 --> 2 - Attempt to assign property of non-object in /Applications/MAMP/htdocs/cms/fuel/app/classes/controller/home.php on line 26
+Warning - 2012-04-30 21:35:58 --> Fuel\Core\Request::execute - The Controller_home::after() method should accept and return the Controller's response, empty return for the after() method is deprecated.
+Warning - 2012-04-30 21:35:58 --> Fuel\Core\Request::execute - The Controller_home controller should return a string or a Response object, support for the $controller->response object is deprecated.
+Error - 2012-04-30 21:38:50 --> 8 - Undefined variable: title in /Applications/MAMP/htdocs/cms/fuel/app/views/dro.php on line 15
+Error - 2012-04-30 21:38:50 --> Error - Could not find asset: script.js in /Applications/MAMP/htdocs/cms/fuel/core/classes/asset.php on line 154
+Error - 2012-04-30 21:39:58 --> 8 - Undefined variable: title in /Applications/MAMP/htdocs/cms/fuel/app/views/dro.php on line 15
